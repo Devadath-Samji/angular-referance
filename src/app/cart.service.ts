09a92d409740e26a9b609d15dc34452113ae292d
Devadath-Samji <devadathsamji@gmail.com>
2020-12-21 00:56:41 +0530

    3 files modified, 2 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,21 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-// @Injectable({
-//   providedIn:'root'
-// })
+//If not mentioned in Providers then use the following @Injectible 
+@Injectable({
+   providedIn:'root'
+  })
 //Services provide logic and data that can shared across components.
 export class CartService {
 items = [];
 // String items[];
-  constructor() { }
+//Dependency Injection
+  constructor(private http: HttpClient) { }
 //business functions
 addToCartInService(product){
     // window.alert('Service Your product has been added to the cart!');
-alert(product.name)
-alert("Javascript product object : " + product)
+// alert(product.name)
+console.log("Product added to cart" + product.name)
+// alert("Javascript product object : " + product)
 //  arrayList.add(product);
 this.items.push(product);
-    alert("Array Object " + this.items);
+    // alert("Array Object " + this.items);
 }
 
 getItems(){
@@ -25,6 +29,10 @@ getItems(){
 clearCart(){
   this.items = [];
   return this.items;
+}
+
+getShippingPrices(){
+  return this.http.get('assets/shipping.json');
 }
 
 }
